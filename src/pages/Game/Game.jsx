8a2532e66648bee311c8233496fd179d97a9ef79
adiaconu55 <adiaconu55@gmail.com>
2022-01-21 18:37:12 +0200

    added dice logic and countdown

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -7,13 +7,55 @@ import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Dice from "react-dice-roll";
+import Countdown from 'react-countdown';
 
 const Game = ({ roomData }) => {
   const users = roomData.users;
+  const Completionist = () => {
+    setDisableDice1(true);
+    setDisableDice2(true);
+    return (<span>Time's Up!</span>);
+  }
+  //pentru o valoare de 1000 este un timer de 1 secunda
+  let [timerValue, setTimerValue] = React.useState(0);
+
+  let [diceValue, setDiceValue] = React.useState(0);
+  let [disableDice1, setDisableDice1] = React.useState(false);
+  let [disableDice2, setDisableDice2] = React.useState(false);
+
+  function diceRoll1(value){
+    setDiceValue(0);
+    setDiceValue(diceValue+=value);
+    setDisableDice1(true);
+  };
+
+  function diceRoll2(value){
+  setDiceValue(diceValue+=value);
+  setDisableDice2(true);
+  };
+
+  const handeRoundStart = () => {
+    setTimerValue(10000);
+  };
 
   return (
     <div>
       <h1 class="round-game">ROUND 1</h1>
+      <div className="dice">
+      <h1><Countdown onStart = {()=>{
+        setDisableDice1(false);
+        setDisableDice2(false);
+      }} date={Date.now() + timerValue} autoStart={false}><Completionist /></Countdown></h1>
+      <br></br><br></br>
+      <Dice onRoll={(value) => diceRoll1(value)} disabled	= {disableDice1}/>
+      {/* aici trebuie spatiu intre zariuri */}
+      &nbsp&nbsp&nbsp
+      {/* dar nu cu &nbsp ca apare scrisul */}
+      <Dice onRoll={(value) => diceRoll2(value)} disabled	= {disableDice2}/>
+      <br></br> <br></br>
+      <h1>Score: {diceValue}</h1>
+      </div>
       <div class="participants-list">
         <h2>ROOM {roomData.name}</h2>
         <br></br>
@@ -37,6 +79,11 @@ const Game = ({ roomData }) => {
               </div>
             ))}
         </List>
+      <div className="center_all">
+        <button className="sign center-all" onClick={handeRoundStart}>
+          <b>Start Round!</b>
+        </button>
+      </div>
       </div>
     </div>
   );

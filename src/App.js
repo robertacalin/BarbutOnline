cc43016450d279214216533caf892fa58e04e631
laviniamnedelea <lavinia.mndl@gmail.com>
2022-01-22 00:34:45 +0200

    Log In anonymously + Log out

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router";
-import { getAuth } from "firebase/auth"; // Firebase v9+
+import { getAuth, onAuthStateChanged } from "firebase/auth"; // Firebase v9+
 import { getDatabase } from "firebase/database"; // Firebase v9+\
 import {
   FirebaseAppProvider,
@@ -14,14 +14,18 @@ import Login from "./pages/Login/Login";
 import Room from "./pages/Room/Room";
 import Game from "./pages/Game/Game";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 function App() {
   const app = useFirebaseApp(); // a parent component contains a `FirebaseAppProvider`
 
   // initialize Database and Auth with the normal Firebase SDK functions
   const database = getDatabase(app);
   const auth = getAuth(app);
-  const user = useSelector((state) => state.user);
-  const anonymous = auth.currentUser ? auth.currentUser.isAnonymous : false;
+  const [currentUser, setCurrentUser] = useState(null);
+
+  onAuthStateChanged(auth, (user) => {
+    setCurrentUser(user);
+  });
 
   return (
     <AuthProvider sdk={auth}>
@@ -30,13 +34,11 @@ function App() {
           <Route
             path="/"
             exact
-            element={
-              user.uid || anonymous ? <Home /> : <Navigate to="/login" />
-            }
+            element={currentUser ? <Home /> : <Navigate to="/login" />}
           />
           <Route
             path="/login"
-            element={!user.uid && !anonymous ? <Login /> : <Navigate to="/" />}
+            element={!currentUser ? <Login /> : <Navigate to="/" />}
           />
           <Route path="/room/:id" element={<Room />} />
           <Route path="/game/:id" element={<Game />} />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,12 +10,15 @@ import Card from "@mui/material/Card";
 import Box from "@mui/material/Box";
 import "./Home.css";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "reactfire";
+import { signOut } from "firebase/auth";
 
 const Home = () => {
   const [open, setOpen] = React.useState(false);
   const [openCreate, setOpenCreate] = React.useState(false);
   const [roomId, setRoomId] = React.useState(null);
   const navigate = useNavigate();
+  const auth = useAuth();
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -39,6 +42,12 @@ const Home = () => {
     setOpenCreate(false);
   };
 
+  const handleLogOut = () => {
+    signOut(auth).then(() => {
+      navigate("/login");
+    });
+  };
+
   return (
     <div>
       <div id="text_div center_all">
@@ -49,6 +58,9 @@ const Home = () => {
             </button>
             <button className="homeButton" onClick={handleClickOpen}>
               <b>Join room</b>
+            </button>
+            <button className="homeButton" onClick={handleLogOut}>
+              <b>Log Out</b>
             </button>
           </div>
           <Dialog maxWidth="md" open={open} onClose={handleClose}>
